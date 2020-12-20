b40bfc79117181f2ca7c5d9fdaaf6ed938adb267
ruoyu <757224439@qq.com>
2020-12-20 10:06:01 +0800

    finish category

--- src/components/novel-m/Category/header.vue
+++ src/components/novel-m/Category/header.vue
@@ -14,19 +14,47 @@
     placeholder="输入作者或书名"
     input-align="center"></van-search>
 
-    
+    <div :class="{'menu-mask':true,          'diy-active': state.menuShow}"></div>
+    <div :class="{'menu-operation-box':true, 'diy-active': state.menuShow}">
+      <div class="menu-operation">
+        <van-row justify="space-around">
+          <van-col @click.stop="$router.push({path: '/'})">
+            <div class="menu-item homepage"
+            :style="{backgroundImage: `url(${require('@assets/img/main-spirit.png')})`}"></div>
+            <div class="menu-item-title">首页</div>
+          </van-col>
+          <van-col  @click.stop="$router.push({path: '/classify'})">
+            <div class="menu-item classify"
+            :style="{backgroundImage: `url(${require('@assets/img/main-spirit.png')})`}"></div>
+            <div class="menu-item-title">分类</div>
+          </van-col>
+          <van-col>
+            <div class="menu-item finish"
+            :style="{backgroundImage: `url(${require('@assets/img/main-spirit.png')})`}"></div>
+            <div class="menu-item-title">完本</div>
+          </van-col>
+        </van-row>
+        <div class="menu-btn">
+          我的书架
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-import { reactive } from 'vue'
-import {goBack} from '@scripts/tools'
+import { onActivated, reactive } from 'vue'
+import { goBack } from '@scripts/tools'
 
 export default {
   setup() {
     const state = reactive({
       menuShow: false,
       search: ''
+    })
+
+    onActivated(() => {
+      state.menuShow = false
     })
 
     return {
@@ -84,5 +112,72 @@ export default {
   background-color: transparent;
   &::before{transform: rotate(45deg);}
   &::after{transform: rotate(-45deg);}
+}
+.menu-mask{
+  visibility: hidden;
+  opacity: 0;
+  position: absolute;
+  left: 0;
+  top: 46px;
+  right: 0;
+  bottom: 0;
+  z-index: 9998;
+  transition: .3s;
+  background-color: rgba(0,0,0,.5);
+}
+.menu-operation-box{
+  visibility: hidden;
+  position: absolute;
+  top: 46px;
+  left: 0;
+  width: 100%;
+  height: 180px;
+  overflow: hidden;
+
+  .menu-operation{
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 16px 8px 16px;
+    width: 100%;
+    z-index: 9999;
+    transform: translateY(-150px);
+    transition: .3s;
+    box-sizing: border-box;
+    background-color: #fff;
+
+    .menu-item{
+      width: 30px;
+      height: 30px;
+      background: #fefefe no-repeat center/cover;
+      background-size: 90px auto;
+
+      &-title{
+        font-size: 14px;
+        text-align: center;
+      }
+    }
+    .homepage{ background-position: -30px 0px; }
+    .classify{ background-position: -60px -28px; }
+    .finish{ background-position: 0px -60px; }
+
+    .menu-btn{
+      margin: 16px auto 0;
+      width: 80%;
+      height: 40px;
+      border-radius: 20px;
+      text-align: center;
+      line-height: 40px;
+      color: #fff;
+      background-color: var(--main-color);
+    }
+  }
+}
+.diy-active{
+  visibility: visible;
+  opacity: 1;
+  .menu-operation{
+    transform: translateY(0);
+  }
 }
 </style>
